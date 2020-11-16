1bf911c02789b54664335414bfbd5191b1493869
rahulriteshqa <rahulriteshqa@gmail.com>
2020-11-16 23:26:47 +0530

    My First Catalon push

---
+++ Test Suites/OrangeHRMSanityTestSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Sanity Test Suite</description>
+   <name>OrangeHRMSanityTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0d897cc2-7ba1-4a62-b975-3d486d2f114d</testSuiteGuid>
+</TestSuiteEntity>
